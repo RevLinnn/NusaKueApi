57a3d67f9083286c2b3712cace7cca686230059a
RevLinnn <vasyahchain16@gmail.com>
2025-05-30 03:30:16 +0700

    add model

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const  recommendationRoutes = require('../routes/recommendation-route.js');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
