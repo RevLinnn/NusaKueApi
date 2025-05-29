eac581d4989a2ef211d32c9d7195261cc8437431
RevLinnn <vasyahchain16@gmail.com>
2025-05-30 03:43:08 +0700

    key secret update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,26 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
-const serviceAccount = require('./firebase-key.json');
+const fs = require('fs');
+
+const serviceAccountPath = '/secrets/firebasekey.json';
+
+let serviceAccount;
+try {
+  const serviceAccountJSON = fs.readFileSync(serviceAccountPath, 'utf8');
+  serviceAccount = JSON.parse(serviceAccountJSON);
+} catch (err) {
+  console.error('Gagal membaca service account dari secret mount:', err);
+  process.exit(1);
+}
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'resonant-forge-456915-j1.firebasestorage.app'
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'resonant-forge-456915-j1.firebasestorage.app'
 });
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
 
 module.exports = {
-    db,
-    bucket
+  db,
+  bucket
 };
